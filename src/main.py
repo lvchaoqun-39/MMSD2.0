560610cb20ef0e7bc746b79b33577e189d3a458e
lvchaoqun-39 <lvchaoqun25@gmail.com>
2025-12-24 19:35:20 +0800

    运行命令

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import os
 os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7897'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7897'
 # os.environ["CUDA_VISIBLE_DEVICES"] = '2'
-
-
+# C:\Users\lvcha\miniconda3\envs\mmsd2\python.exe src/main.py --model MV_CLIP --text_name text_json_final --weight_decay 0.05 --train_batch_size 32 --dev_batch_size 32 --learning_rate 5e-4 --clip_learning_rate 1e-6 --num_train_epochs 10 --layers 3 --max_grad_norm 5 --dropout_rate 0.1 --optimizer_name adam --text_size 512 --image_size 768 --warmup_proportion 0.2 --device -1
+# C:\Users\lvcha\miniconda3\envs\mmsd2\python.exe src/main.py --model MV_CLIP --text_name text_json_final --weight_decay 0.05 --train_batch_size 32 --dev_batch_size 32 --learning_rate 5e-4 --clip_learning_rate 1e-6 --num_train_epochs 10 --layers 3 --max_grad_norm 5 --dropout_rate 0.1 --optimizer_name adam --text_size 512 --image_size 768 --warmup_proportion 0.2 --device -1 --limit 100
+# C:\Users\lvcha\miniconda3\envs\mmsd2\python.exe src/main.py --device 0 
 
 from model import MV_CLIP
 from train import train
@@ -63,6 +64,7 @@ def main():
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = args.device
     device = torch.device("cuda" if torch.cuda.is_available() and int(args.device) >= 0 else "cpu")
+    print(f"Using device: {device}")
 
     seed_everything(args.seed)
 
