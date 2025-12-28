2ae058f49ae1fb6049bb89b9edcf27c693f0d399
lvchaoqun-39 <lvchaoqun25@gmail.com>
2025-12-28 23:35:32 +0800

    注释

--- src/model.py
+++ src/model.py
@@ -56,28 +56,28 @@ class MV_CLIP(nn.Module):
 
     def forward(self, inputs, labels):
         output = self.model(**inputs,output_attentions=True)
-        text_features = output['text_model_output']['last_hidden_state']
-        image_features = output['vision_model_output']['last_hidden_state']
-        text_feature = output['text_model_output']['pooler_output']
-        image_feature = output['vision_model_output']['pooler_output']
-        text_feature = self.text_linear(text_feature)
-        image_feature = self.image_linear(image_feature)
+        text_features = output['text_model_output']['last_hidden_state'] # 文本特征
+        image_features = output['vision_model_output']['last_hidden_state'] # 图像特征
+        text_feature = output['text_model_output']['pooler_output'] # 文本池化特征
+        image_feature = output['vision_model_output']['pooler_output'] # 图像池化特征
+        text_feature = self.text_linear(text_feature) # 文本特征线性变换
+        image_feature = self.image_linear(image_feature) # 图像特征线性变换
 
-        text_embeds = self.model.text_projection(text_features)
-        image_embeds = self.model.visual_projection(image_features)
-        input_embeds = torch.cat((image_embeds, text_embeds), dim=1)
-        attention_mask = torch.cat((torch.ones(text_features.shape[0], 50).to(text_features.device), inputs['attention_mask']), dim=-1)
-        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
-        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)
-        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
-        fuse_hiddens, all_attentions = self.trans(input_embeds, extended_attention_mask, output_all_encoded_layers=False)
-        fuse_hiddens = fuse_hiddens[-1]
-        new_text_features = fuse_hiddens[:, 50:, :]
+        text_embeds = self.model.text_projection(text_features) # 文本特征投影
+        image_embeds = self.model.visual_projection(image_features) # 图像特征投影
+        input_embeds = torch.cat((image_embeds, text_embeds), dim=1) # 合并文本特征和图像特征成为多模态嵌入特征
+        attention_mask = torch.cat((torch.ones(text_features.shape[0], 50).to(text_features.device), inputs['attention_mask']), dim=-1) # 合并文本特征和图像特征的注意力掩码
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2) # 扩展掩码维度，适配Transformer注意力计算
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype) # 将注意力掩码转换为与模型参数 相同的数据类型
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0 # 将注意力掩码转换为 注意力分数屏蔽值
+        fuse_hiddens, all_attentions = self.trans(input_embeds, extended_attention_mask, output_all_encoded_layers=False) # 多模态特征融合的核心步骤 ，通过自定义的Transformer编码器将图像和文本特征进行深度交互融合，输出融合后的隐藏状态和所有层的注意力矩阵。
+        fuse_hiddens = fuse_hiddens[-1] # 取 Transformer 编码器的最后一层输出作为融合后的隐藏状态
+        new_text_features = fuse_hiddens[:, 50:, :] # 提取文本部分的融合特征，从第 50 个 token 开始到结束
         new_text_feature = new_text_features[
             torch.arange(new_text_features.shape[0], device=inputs['input_ids'].device), inputs['input_ids'].to(torch.int).argmax(dim=-1)
-        ]
+        ] # 提取文本序列的全局特征，从文本序列特征中选择 最后一个有效token的特征 作为整个文本的融合表示
 
-        new_image_feature = fuse_hiddens[:, 0, :].squeeze(1)
+        new_image_feature = fuse_hiddens[:, 0, :].squeeze(1) # 提取图像部分的融合特征，取第 0 个 token 的特征（CLS 令牌）
 
         text_weight = self.att(new_text_feature)
         image_weight = self.att(new_image_feature)    
