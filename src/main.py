962729acfcdfb287a5224a31b036fe0d94ae8c0b
lvchaoqun-39 <lvchaoqun25@gmail.com>
2026-01-04 23:41:54 +0800

    update：添加属性便于调参

--- src/main.py
+++ src/main.py
@@ -43,6 +43,8 @@ def set_args():
     parser.add_argument('--weight_decay', default=0.05, type=float, help='weight decay') # 权重衰减（防止过拟合）
     parser.add_argument('--warmup_proportion', default=0.2, type=float, help='warm up proportion') # 预热比例
     parser.add_argument('--dropout_rate', default=0.1, type=float, help='dropout rate') # 丢弃率（随机丢弃神经元）
+    parser.add_argument('--fim_top_k', default=5, type=int, help='top k for FIM') # FIM的top k参数
+    parser.add_argument('--fim_dynrt_iters', default=1, type=int, help='dynamic routing iterations for FIM') # FIM的动态路由迭代次数
     parser.add_argument('--output_dir', default='../output_dir/', type=str, help='the output path') # 输出路径
     parser.add_argument('--limit', default=None, type=int, help='the limited number of training examples') # 训练样本数量限制
     parser.add_argument('--seed', type=int, default=42, help='random seed') # 随机种子
