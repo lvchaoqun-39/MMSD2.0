15a0b5bac0c3b5e87259687921cf7babc91a6f1f
lvchaoqun <lvchaoqun@bytedance.com>
2026-02-22 10:20:10 +0800

    update:改随机数种子

--- src/main.py
+++ src/main.py
@@ -64,7 +64,7 @@ def set_args():
     parser.add_argument('--fp16', default=0, type=int, help='use torch autocast fp16 on cuda')
     parser.add_argument('--output_dir', default='../output_dir/', type=str, help='the output path') # 输出路径
     parser.add_argument('--limit', default=None, type=int, help='the limited number of training examples') # 训练样本数量限制
-    parser.add_argument('--seed', type=int, default=43, help='random seed') # 随机种子
+    parser.add_argument('--seed', type=int, default=37, help='random seed') # 随机种子
     return parser.parse_args()
 
 
